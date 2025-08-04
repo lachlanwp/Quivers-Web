712cc0ed8ae4329ab8abcee6796e6d9427f78e3f
Lachlan P <lwpearce@gmail.com>
2025-08-04 19:17:03 +1000

    New menu links, drummer update + GA

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+// Google Analytics 4 Tracking Code
+// Initialize Google Analytics with measurement ID G-YMG0NST2G1
+(function (i, s, o, g, r, a, m) {
+  i["GoogleAnalyticsObject"] = r;
+  (i[r] =
+    i[r] ||
+    function () {
+      (i[r].q = i[r].q || []).push(arguments);
+    }),
+    (i[r].l = 1 * new Date());
+  (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m);
+})(
+  window,
+  document,
+  "script",
+  "https://www.google-analytics.com/analytics.js",
+  "ga"
+);
+
+ga("create", "G-YMG0NST2G1", "auto");
+ga("send", "pageview");
+
 // Mobile navigation toggle
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
