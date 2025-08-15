cf31abf9fa214f47fdfc29a56b1dc6611af842b9
Lachlan P <lwpearce@gmail.com>
2025-08-16 08:43:47 +1000

    Fix Google Analytics

--- script.js
+++ script.js
@@ -1,27 +1,12 @@
 // Google Analytics 4 Tracking Code
 // Initialize Google Analytics with measurement ID G-YMG0NST2G1
-(function (i, s, o, g, r, a, m) {
-  i["GoogleAnalyticsObject"] = r;
-  (i[r] =
-    i[r] ||
-    function () {
-      (i[r].q = i[r].q || []).push(arguments);
-    }),
-    (i[r].l = 1 * new Date());
-  (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
-  a.async = 1;
-  a.src = g;
-  m.parentNode.insertBefore(a, m);
-})(
-  window,
-  document,
-  "script",
-  "https://www.google-analytics.com/analytics.js",
-  "ga"
-);
-
-ga("create", "G-YMG0NST2G1", "auto");
-ga("send", "pageview");
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "G-YMG0NST2G1");
+// End Google Analytics 4 Tracking Code
 
 // Mobile navigation toggle
 const hamburger = document.querySelector(".hamburger");
